4dd70e616abf786d95f9b1b9b712acf4b8702097
aaryanayakk <aaryan1nayak@gmail.com>
2026-09-07 00:22:32 +0530

    Update Palindrome.java

--- Practice Problems/Palindrome.java
+++ Practice Problems/Palindrome.java
@@ -1,1 +1,32 @@
+import java.util.Scanner;
 
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.nextLine();
+
+        checkPalindrome(word);
+    }
+
+    static void checkPalindrome(String word) {
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < word.length() / 2; i++) {
+
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
