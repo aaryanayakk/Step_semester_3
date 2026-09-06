2740cb3ac1140d13e800c8d33a225022faa83533
aaryanayakk <aaryan1nayak@gmail.com>
2026-09-07 01:00:45 +0530

    Add Employee class with salary raise functionality
    Implemented Employee class with salary management.

--- Practice Problem/Employee.java
+++ Practice Problem/Employee.java
@@ -1,1 +1,37 @@
+public class Employee {
 
+    String employeeId;
+    double salary;
+
+    public Employee(String employeeId, double salary) {
+        this.employeeId = employeeId;
+        this.salary = salary;
+    }
+
+    public void raiseSalary(double salary) {
+        this.salary = this.salary + salary;
+    }
+
+    public static void main(String[] args) {
+
+        Employee[] employees = {
+            new Employee("E-101", 40000),
+            new Employee("E-102", 55000),
+            new Employee("E-103", 62000),
+            new Employee("E-104", 48000)
+        };
+
+        double bonus = 5000;
+
+        for (int i = 0; i < employees.length; i++) {
+
+            employees[i].raiseSalary(bonus);
+
+            System.out.println(
+                employees[i].employeeId
+                + " | Final Salary: Rs "
+                + employees[i].salary
+            );
+        }
+    }
+}
