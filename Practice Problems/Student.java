279edbab32313b5e695db6d3fdbf2273a75455b9
aaryanayakk <aaryan1nayak@gmail.com>
2026-09-07 00:48:52 +0530

    Implement Student class with college info methods

--- Practice Problems/Student.java
+++ Practice Problems/Student.java
@@ -1,1 +1,36 @@
+public class Student {
 
+    public static void main(String[] args) {
+
+        Student student1 =
+                new Student("Ravi", 85);
+
+        Student student2 =
+                new Student("Anitha", 90);
+
+        Student.printCollegeInfo();
+    }
+
+    String name;
+    int attendance;
+
+    static String collegeName =
+            "SRM Institute of Science and Technology";
+
+    static int studentCount = 0;
+
+    Student(String name, int attendance) {
+
+        this.name = name;
+        this.attendance = attendance;
+
+        studentCount++;
+    }
+
+    static void printCollegeInfo() {
+
+        System.out.println(collegeName);
+        System.out.println("Students created: "
+                + studentCount);
+    }
+}
