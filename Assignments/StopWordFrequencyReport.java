6f6a19ffd318b70db31c2156e0a439edcc03ac11
aaryanayakk <aaryan1nayak@gmail.com>
2026-09-07 00:20:00 +0530

    Update StopWordFrequencyReport.java

--- Assignments/StopWordFrequencyReport.java
+++ Assignments/StopWordFrequencyReport.java
@@ -1,1 +1,67 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class StopWordFrequencyReport {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String feedback = sc.nextLine();
+
+        printFilteredWordFrequency(feedback);
+    }
+
+    static void printFilteredWordFrequency(String feedback) {
+
+        String cleanedText = feedback.toLowerCase();
+
+        cleanedText = cleanedText.replace(".", "");
+        cleanedText = cleanedText.replace(",", "");
+
+        String[] words = cleanedText.split("\\s+");
+
+        Set<String> stopWords = new HashSet<>();
+
+        stopWords.add("the");
+        stopWords.add("was");
+        stopWords.add("and");
+        stopWords.add("a");
+        stopWords.add("is");
+        stopWords.add("of");
+        stopWords.add("in");
+
+        HashMap<String, Integer> frequency = new HashMap<>();
+
+        for (String word : words) {
+
+            if (!stopWords.contains(word)) {
+
+                if (frequency.containsKey(word)) {
+                    frequency.put(word, frequency.get(word) + 1);
+                } else {
+                    frequency.put(word, 1);
+                }
+            }
+        }
+
+        List<Map.Entry<String, Integer>> list =
+                new ArrayList<>(frequency.entrySet());
+
+        Collections.sort(list, (a, b) ->
+                b.getValue() - a.getValue());
+
+        for (Map.Entry<String, Integer> entry : list) {
+
+            System.out.println(
+                entry.getKey() + ": " + entry.getValue()
+            );
+        }
+    }
+}
