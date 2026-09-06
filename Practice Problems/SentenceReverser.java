3f3152c72d65df66841eb7ecb5fa4dcb0e3de051
aaryanayakk <aaryan1nayak@gmail.com>
2026-09-07 00:24:17 +0530

    Update SentenceReverser.java

--- Practice Problems/SentenceReverser.java
+++ Practice Problems/SentenceReverser.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
 
+public class SentenceReverser {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String sentence = sc.nextLine();
+
+        String result = reverseSentence(sentence);
+
+        System.out.println(result);
+    }
+
+    static String reverseSentence(String sentence) {
+
+        String[] words = sentence.split(" ");
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+
+            result.append(words[i]);
+
+            if (i != 0) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+}
