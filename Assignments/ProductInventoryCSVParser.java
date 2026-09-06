f6254a985b6d8ad763b971d02191ca580fea36a6
aaryanayakk <aaryan1nayak@gmail.com>
2026-09-07 00:18:15 +0530

    Update ProductInventoryCSVParser.java

--- Assignments/ProductInventoryCSVParser.java
+++ Assignments/ProductInventoryCSVParser.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
 
+public class ProductInventoryCSVParser {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String csvLine = sc.nextLine();
+
+        parseInventoryRecord(csvLine);
+    }
+
+    static void parseInventoryRecord(String csvLine) {
+
+        String[] fields = csvLine.split(",");
+
+        if (fields.length != 3) {
+
+            System.out.println("Invalid Record");
+
+        } else {
+
+            System.out.println(
+                "Product: " + fields[0]
+                + " | SKU: " + fields[1]
+                + " | Qty: " + fields[2]
+            );
+        }
+    }
+}
