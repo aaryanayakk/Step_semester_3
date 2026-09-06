0d87e62daf650b0ddc2a7e4c12af5fb836339710
aaryanayakk <aaryan1nayak@gmail.com>
2026-09-07 00:57:33 +0530

    Update Payment.java

--- Assignment/Payment.java
+++ Assignment/Payment.java
@@ -1,1 +1,60 @@
+public class Payment {
 
+    public void pay(double amount) {
+        System.out.println("Paid (cash): Rs " + amount);
+    }
+
+    public static void main(String[] args) {
+
+        Payment[] payments = {
+            new CardPayment(),
+            new Payment(),
+            new CardPayment(),
+            new Payment(),
+            new CardPayment()
+        };
+
+        double[] amounts = {
+            100, 50, 200, 75, 120
+        };
+
+        double totalCollected = 0;
+
+        for (int i = 0; i < payments.length; i++) {
+
+            if (payments[i] instanceof CardPayment) {
+
+                CardPayment cardPayment =
+                        (CardPayment) payments[i];
+
+                double charged =
+                        cardPayment.payWithProcessingFee(amounts[i]);
+
+                totalCollected = totalCollected + charged;
+
+            } else {
+
+                payments[i].pay(amounts[i]);
+
+                totalCollected =
+                        totalCollected + amounts[i];
+            }
+        }
+
+        System.out.println(
+                "Total Collected: Rs " + totalCollected);
+    }
+}
+
+class CardPayment extends Payment {
+
+    public double payWithProcessingFee(double amount) {
+
+        double total = amount + (amount * 0.02);
+
+        System.out.println(
+                "Charged (card, incl. fee): Rs " + total);
+
+        return total;
+    }
+}
