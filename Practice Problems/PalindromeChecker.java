089dc47717858b4c357a6380efdd1b74dccfbcc9
aaryanayakk <aaryan1nayak@gmail.com>
2026-09-06 23:46:55 +0530

    Implement palindrome checking methods
    Added methods to check for palindromes using iterative, recursive, and array reversal approaches.

--- Practice Problems/PalindromeChecker.java
+++ Practice Problems/PalindromeChecker.java
@@ -1,1 +1,74 @@
+import java.util.Scanner;
 
+public class PalindromeChecker {
+
+    public static boolean isPalindromeIterative(String text) {
+
+        int start = 0;
+        int end = text.length() - 1;
+
+        while (start < end) {
+
+            if (text.charAt(start) != text.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static boolean isPalindromeRecursive(String text) {
+
+        if (text.length() <= 1) {
+            return true;
+        }
+
+        if (text.charAt(0) != text.charAt(text.length() - 1)) {
+            return false;
+        }
+
+        return isPalindromeRecursive(text.substring(1, text.length() - 1));
+    }
+
+    public static boolean isPalindromeArrayReversal(String text) {
+
+        char[] characters = text.toCharArray();
+
+        for (int i = 0; i < characters.length / 2; i++) {
+
+            char temp = characters[i];
+            characters[i] = characters[characters.length - 1 - i];
+            characters[characters.length - 1 - i] = temp;
+        }
+
+        String reversedText = new String(characters);
+
+        return text.equals(reversedText);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a text: ");
+        String text = sc.next();
+
+        boolean iterativeResult = isPalindromeIterative(text);
+        boolean recursiveResult = isPalindromeRecursive(text);
+        boolean arrayResult = isPalindromeArrayReversal(text);
+
+        System.out.println("Iterative: " +
+                (iterativeResult ? "Palindrome" : "Not Palindrome"));
+
+        System.out.println("Recursive: " +
+                (recursiveResult ? "Palindrome" : "Not Palindrome"));
+
+        System.out.println("Array Reversal: " +
+                (arrayResult ? "Palindrome" : "Not Palindrome"));
+
+        sc.close();
+    }
+}
