0b99d3b9b76b60dcd047ba15094ce1668876ed8d
aaryanayakk <aaryan1nayak@gmail.com>
2026-09-07 00:23:08 +0530

    Implement Caesar Cipher encryption

--- Practice Problems/CaesarCipher.java
+++ Practice Problems/CaesarCipher.java
@@ -1,1 +1,45 @@
+import java.util.Scanner;
 
+public class CaesarCipher {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.nextLine();
+
+        String result = encryptWord(word);
+
+        System.out.println(result);
+    }
+
+    static String encryptWord(String word) {
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+
+            char ch = word.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                ch = (char) (ch + 1);
+
+                if (ch > 'z') {
+                    ch = 'a';
+                }
+            }
+
+            else if (ch >= 'A' && ch <= 'Z') {
+                ch = (char) (ch + 1);
+
+                if (ch > 'Z') {
+                    ch = 'A';
+                }
+            }
+
+            result.append(ch);
+        }
+
+        return result.toString();
+    }
+}
