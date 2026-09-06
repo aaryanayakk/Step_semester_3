58e61d326ac832ff12b0fa01a473b20e517a282e
aaryanayakk <aaryan1nayak@gmail.com>
2026-09-07 00:24:50 +0530

    Update CSVParser.java

--- Practice Problems/CSVParser.java
+++ Practice Problems/CSVParser.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class CSVParser {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String csv = sc.nextLine();
+
+        printFields(csv);
+    }
+
+    static void printFields(String csv) {
+
+        String[] fields = csv.split(",");
+
+        for (String field : fields) {
+            System.out.println(field);
+        }
+    }
+}
