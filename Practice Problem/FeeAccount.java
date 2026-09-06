b897ee31586d50b95ee3912ae81b2ba46c18a549
aaryanayakk <aaryan1nayak@gmail.com>
2026-09-07 01:03:02 +0530

    Update FeeAccount.java

--- Practice Problem/FeeAccount.java
+++ Practice Problem/FeeAccount.java
@@ -1,1 +1,59 @@
+public class FeeAccount {
 
+    public void processPayment(double amount) {
+        System.out.println(
+            "Paid in one go (day-scholar account)"
+        );
+    }
+
+    public static void main(String[] args) {
+
+        FeeAccount[] accounts = {
+            new HostelFeeAccount(),
+            new HostelFeeAccount(),
+            new FeeAccount(),
+            new FeeAccount()
+        };
+
+        double amount = 60000;
+
+        int hostelCount = 0;
+        int dayScholarCount = 0;
+
+        for (int i = 0; i < accounts.length; i++) {
+
+            if (accounts[i] instanceof HostelFeeAccount) {
+
+                HostelFeeAccount hostel =
+                    (HostelFeeAccount) accounts[i];
+
+                hostel.processPayment(amount);
+
+                hostelCount++;
+
+            } else {
+
+                accounts[i].processPayment(amount);
+
+                dayScholarCount++;
+            }
+        }
+
+        System.out.println(
+            "Hostel accounts processed: "
+            + hostelCount
+            + " | Day-scholar accounts processed: "
+            + dayScholarCount
+        );
+    }
+}
+
+class HostelFeeAccount extends FeeAccount {
+
+    @Override
+    public void processPayment(double amount) {
+        System.out.println(
+            "Paid in two installments (hostel account)"
+        );
+    }
+}
